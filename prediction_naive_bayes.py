98f6d3184b48d5a49d5429409ef53845c5f2b164
Teriyaki <103395356+PauThing@users.noreply.github.com>
2023-11-19 21:00:04 +0800

    Exploration and Preprocess Updates

--- prediction_naive_bayes.py
+++ prediction_naive_bayes.py
@@ -18,44 +18,12 @@ nltk.download('stopwords')
 # load the dataset
 tweets_df = pd.read_csv('./Datasets/tweets_train.csv')
 
-# remove quotes at the beginning and end of the text field
-tweets_df['text'] = tweets_df['text'].str.strip('\"')
-tweets_df['selected_text'] = tweets_df['selected_text'].str.strip('\"')
+# explore the data
+# display summary statistics
+print(tweets_df.describe())
 
-# drop the 'textID' and 'selected_text' columns
-tweets_df.drop(['textID'], axis=1, inplace=True)
-
-# reset the index of DataFrame after dropping columns
-tweets_df.reset_index(drop=True, inplace=True)
-
-# assign the column name 'sentiment' to the variable
-target = 'sentiment'
-
-# create a deep copy of the DataFrame df 
-df = tweets_df.copy(deep=True)
-
-# split the text into individual words or tokens
-tweets_df['text_tokens'] = tweets_df['selected_text'].apply(word_tokenize)
-
-# convert the text data into numerical vectors using TF-IDF representation
-# map sentiment labels to numerical values for classification
-# create a TF-IDF vectorizer
-tfidf_vectorizer = TfidfVectorizer(max_features=10000, ngram_range=(1, 2))
-
-# remove common words that do not carry much meaning
-stop_words = set(stopwords.words('english'))
-tweets_df['text_tokens'] = tweets_df['text_tokens'].apply(lambda tokens: [word for word in tokens if word.lower() not in stop_words])
-
-# reduce words to their base or root form
-stemmer = PorterStemmer()
-tweets_df['text_tokens'] = tweets_df['text_tokens'].apply(lambda tokens: [stemmer.stem(word) for word in tokens])
-
-# fit and transform the text data
-X_tfidf = tfidf_vectorizer.fit_transform(tweets_df['text_tokens'].apply(lambda x: ' '.join(x)))
-
-# convert the sentiment labels to numerical values
-label_mapping = {'positive': 1, 'negative': -1, 'neutral': 0}
-y = tweets_df[target].map(label_mapping)
+# check for empty elements
+print(tweets_df.isnull().sum())
 
 # display the positive words as WordCloud image
 # text = " ".join(i for i in tweets_df[tweets_df[target]=='positive']['selected_text'])
@@ -91,6 +59,46 @@ y = tweets_df[target].map(label_mapping)
 # plt.pie(tweets_df[target].value_counts(), labels=['Neutral','Positive','Negative'], counterclock=False, shadow=True, 
 #         explode=[0,0,0.08], autopct='%1.1f%%', radius=1, startangle=0)
 # plt.show()
+
+# preprocess the data
+# remove quotes at the beginning and end of the text field
+tweets_df['text'] = tweets_df['text'].str.strip('\"')
+tweets_df['selected_text'] = tweets_df['selected_text'].str.strip('\"')
+
+# drop the 'textID' and 'selected_text' columns
+tweets_df.drop(['textID'], axis=1, inplace=True)
+
+# reset the index of DataFrame after dropping columns
+tweets_df.reset_index(drop=True, inplace=True)
+
+# create a deep copy of the DataFrame df 
+df = tweets_df.copy(deep=True)
+
+# assign the column name 'sentiment' to the variable
+target = 'sentiment'
+
+# split the text into individual words or tokens
+tweets_df['text_tokens'] = tweets_df['selected_text'].apply(word_tokenize)
+
+# remove common words that do not carry much meaning
+stop_words = set(stopwords.words('english'))
+tweets_df['text_tokens'] = tweets_df['text_tokens'].apply(lambda tokens: [word for word in tokens if word.lower() not in stop_words])
+
+# reduce words to their base or root form
+stemmer = PorterStemmer()
+tweets_df['text_tokens'] = tweets_df['text_tokens'].apply(lambda tokens: [stemmer.stem(word) for word in tokens])
+
+# convert the text data into numerical vectors using TF-IDF representation
+# map sentiment labels to numerical values for classification
+# create a TF-IDF vectorizer
+tfidf_vectorizer = TfidfVectorizer(max_features=10000, ngram_range=(1, 2))
+
+# fit and transform the text data
+X_tfidf = tfidf_vectorizer.fit_transform(tweets_df['text_tokens'].apply(lambda x: ' '.join(x)))
+
+# convert the sentiment labels to numerical values
+label_mapping = {'positive': 1, 'negative': -1, 'neutral': 0}
+y = tweets_df[target].map(label_mapping)
 
 # split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X_tfidf, y, test_size=0.2, random_state=42)
